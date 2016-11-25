c8f95380ae0d52a494a6e7b7239e9899925a4bab
snlia <chinsnlia@gmail.com>
2016-11-25 16:39:36 +0800

    now u can set parameters, see --help for detail

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <boost/program_options.hpp>
 
 #define sqr(x) ((x) * (x))
 
 using namespace std;
 using namespace cv;
+namespace po = boost::program_options;
 
 enum {UP, RIGHT, DOWN, LEFT};
 
@@ -14,6 +16,13 @@ vector<Vec4i> hierarchy;
 vector<vector<Point>> contours;
 Mat frame;
 int f [1000000];
+
+bool useblur = 1;
+int cannylow = 75;
+int cannyhigh = 200;
+int qrsize = 100;
+double areathre = 0.005;
+double distthre = 0.2;
 
 double crossProduct (Point2f A, Point2f B, Point2f C) {
     return ((B.x - A.x) * (C.y - B.y) - (C.x - B.x) * (B.y - A.y));
@@ -75,15 +84,15 @@ vector<int> getPoint (double AB, double BC, double CA, int A, int B, int C) {
 bool dist_constraint (double AB, double BC, double CA) {
     // TODO : add more constraint
     if (AB > BC && AB > CA) {
-        if (abs (BC - CA) > 0.2 * max (BC, CA)) return 1;
+        if (abs (BC - CA) > distthre * max (BC, CA)) return 1;
         return 0;
     }
     if (BC > AB && BC > CA) {
-        if (abs (AB - CA) > 0.2 * max (CA, AB)) return 1;
+        if (abs (AB - CA) > distthre * max (CA, AB)) return 1;
         return 0;
     }
     if (CA > AB && CA > BC) {
-        if (abs (BC - AB) > 0.2 * max (BC, AB)) return 1;
+        if (abs (BC - AB) > distthre * max (BC, AB)) return 1;
         return 0;
     }
     return 1;
@@ -93,7 +102,7 @@ bool area_constraint (double areaA, double areaB, double areaC) {
     double areaMean = (areaA + areaB + areaC) / 3;
     double sigma = sqr (areaA - areaMean) + sqr (areaB - areaMean) + sqr (areaC - areaMean);
     sigma /= 3 * sqr (areaMean);
-    return sigma > 0.005;
+    return sigma > areathre;
 }
 
 Point2f findAwayFromLine (int x, Point2f A, Point2f B) {
@@ -144,10 +153,9 @@ vector<Point2f> findCorners (int top, int left, int right, Point2f meanTop, Poin
 
 vector<Mat> findQR (vector<int> candidates) {
     vector<Mat> res; res.clear ();
-    Mat qr, raw, gray, thres, rawFrame;
-    qr = Mat::zeros(100, 100, CV_8UC1);
-    raw = Mat::zeros(100, 100, CV_8UC1);
-    rawFrame = frame;
+    Mat qr, raw;
+    qr = Mat::zeros(qrsize, qrsize, CV_8UC1);
+    raw = Mat::zeros(qrsize, qrsize, CV_8UC1);
     int size = candidates.size ();
     if (size < 3) return res; 
     vector<Moments> mome(size);
@@ -175,10 +183,6 @@ vector<Mat> findQR (vector<int> candidates) {
                 // Use cross product to determine left and right
                 if (crossProduct (mean[left], mean[top], mean[right]) < 0) 
                     swap (left, right);
-                // Draw the FIG
-                drawContours (frame, contours, candidates[top] , Scalar(255,0,0), 2, 8, hierarchy, 0 );
-                drawContours (frame, contours, candidates[left] , Scalar(0,255,0), 2, 8, hierarchy, 0 );
-                drawContours (frame, contours, candidates[right] , Scalar(0,0,255), 2, 8, hierarchy, 0 );
                 // Find all corners
                 pts1 = findCorners (candidates[top], candidates[left], candidates[right], mean[top], mean[left], mean[right]);
                 /*
@@ -194,22 +198,76 @@ vector<Mat> findQR (vector<int> candidates) {
 				pts2.push_back(Point2f(qr.cols, qr.rows));
                 // Do perspective transform
                 Mat M = getPerspectiveTransform (pts1,pts2);
-                warpPerspective (rawFrame, raw, M, Size (qr.cols,qr.rows));
+                warpPerspective (frame, raw, M, Size (qr.cols,qr.rows));
                 copyMakeBorder (raw, qr, 10, 10, 10, 10, BORDER_CONSTANT, Scalar(255,255,255));
+
+                // Draw the FIG
+                drawContours (frame, contours, candidates[top] , Scalar(255,0,0), 2, 8, hierarchy, 0 );
+                drawContours (frame, contours, candidates[left] , Scalar(0,255,0), 2, 8, hierarchy, 0 );
+                drawContours (frame, contours, candidates[right] , Scalar(0,0,255), 2, 8, hierarchy, 0 );
+
                 res.push_back (qr);
                 return res;
             }
     return res;
 }
 
+int parameter_init (int argc, const char *argv[]) {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "show this message.")
+        ("noblur", "do not use blur.")
+        ("clow", po::value<double>(), "set up the low thresold of canny, default 75.")
+        ("chigh", po::value<double>(), "set up the high thresold of canny, default 200.")
+        ("size", po::value<int>(), "set up the qr code size, default 100.")
+        ("athre", po::value<double>(), "set up the thresold for area constraint, default 0.2.")
+        ("dthre", po::value<double>(), "set up the thresold for distance constraint, default 0.005.")
+        ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);    
+
+    if (vm.count("help")) {
+        cout << desc << endl;
+        return EOF;
+    }
+    else if (vm.count ("noblur")) {
+        cout << "Do not use blur." << endl;
+        useblur = 0;
+    } 
+    else  if (vm.count ("clow")) {
+        cannylow = vm["clow"].as<double> ();
+        printf ("canny low thresold is set as %lf:\n", cannylow);
+    }
+    else if (vm.count ("chigh")) {
+        cannyhigh = vm["chigh"].as<double> ();
+        printf ("canny high thresold is set as %lf:\n", cannyhigh);
+    }
+    else if (vm.count ("size")) {
+        qrsize = vm["size"].as<int> ();
+        printf ("QRcode size is set as %d:\n", qrsize);
+    }
+    else if (vm.count ("athre")) {
+        areathre = vm["athre"].as<double> ();
+        printf ("area constraint thresold is set as %lf:\n", areathre);
+    }
+    else if (vm.count ("dthre")) {
+        distthre = vm["dthre"].as<double> ();
+        printf ("distance constraint thresold is set as %lf:\n", distthre);
+    }
+    return 0;
+}
+
 int main(int argc, const char *argv[]) {
+
+    if (parameter_init (argc, argv)) return 0;
     
 	VideoCapture capture(0);
 
     capture >> frame;
 
 	Mat gray(frame.size(), CV_MAKETYPE(frame.depth(), 1));
-	Mat traces(frame.size(), CV_8UC3);
+	Mat detected_edges(frame.size(), CV_MAKETYPE(frame.depth(), 1));
 	Mat edges (frame.size (), CV_MAKETYPE (frame.depth (), 1));
 
     cout << "Press any key to return." << endl;
@@ -218,14 +276,15 @@ int main(int argc, const char *argv[]) {
         // Change to grayscale
         cvtColor (frame, gray, CV_RGB2GRAY);
 
+        if (useblur)
+            blur (gray, detected_edges, Size(3,3));
         // Find FIG candidates
-        Canny(frame, edges, 100 , 200, 3);		// Apply Canny edge detection on the gray image
+        Canny (detected_edges, edges, cannylow, cannyhigh, 3);		// Apply Canny edge detection on the gray image
         findContours (edges, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
         vector<int> candidates = findCandidates (gray);
         
         vector<Mat> qrs = findQR (candidates);
 
-		imshow ("Image", frame);
         for (int i = 0; i < qrs.size (); ++i) {
             String Name = "QR";
             String id;
@@ -235,6 +294,8 @@ int main(int argc, const char *argv[]) {
             Name += id;
             imshow (Name, qrs[i]);
         }
+
+		imshow ("Image", frame);
 
         key = waitKey (100);
     }
